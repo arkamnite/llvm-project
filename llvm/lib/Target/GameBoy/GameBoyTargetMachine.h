38988f1b27fd12a792df316e927a791f43c3d012
Akram Ubuntu <akramahmadgb@gmail.com>
2022-08-31 19:27:20 +0100

    Added basic registers

---
+++ llvm/lib/Target/GameBoy/GameBoyTargetMachine.h
@@ -0,0 +1,16 @@
+#ifndef LLVM_LIB_TARGET_GAMEBOY_GAMEBOYTARGETMACHINE_H
+#define LLVM_LIB_TARGET_GAMEBOY_GAMEBOYTARGETMACHINE_H
+
+// #include "GameBoySubTarget.h"
+// #include "MCTargetDesc/GameBoyMCTargetDesc.h"
+
+#include "llvm/CodeGen/Passes.h"
+#include "llvm/CodeGen/SelectionDAGISel.h"
+#include "llvm/CodeGen/TargetFrameLowering.h"
+#include "llvm/Target/TargetMachine.h"
+
+namespace llvm{
+    
+}
+
+#endif // LLVM_LIB_TARGET_GAMEBOY_GAMEBOYTARGETMACHINE_H

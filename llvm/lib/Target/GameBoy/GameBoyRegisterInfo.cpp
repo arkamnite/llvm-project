e6de95dd69919a07d94bf48761bb0cb95873e8de
Akram Ahmad <bulgeypip786@gmail.com>
2022-10-08 04:20:25 +0100

    Abstract class error fixed

--- llvm/lib/Target/GameBoy/GameBoyRegisterInfo.cpp
+++ llvm/lib/Target/GameBoy/GameBoyRegisterInfo.cpp
@@ -12,4 +12,23 @@ namespace llvm {
 
 GameBoyRegisterInfo::GameBoyRegisterInfo() : GameBoyGenRegisterInfo(0) {}
 
+const uint16_t *
+GameBoyRegisterInfo::getCalleeSavedRegs(const MachineFunction *MF) const {
+    return CSR_Normal_SaveList;
+}
+
+BitVector GameBoyRegisterInfo::getReservedRegs(const MachineFunction *MF) const {
+    BitVector Reserved(getNumRegs());
+
+    // TODO: Set the reserved registers here. 
+    return Reserved;
+}
+
+void GameBoyRegisterInfo::eliminateFrameIndex(MachineBasicBlock::iterator II,
+                                              int SPAdj, unsigned FIOperandNum,
+                                              RegScavenger *RS) const {
+
+    return;
+}
+
 } // end namespace llvm
--- llvm/lib/Target/GameBoy/GameBoyRegisterInfo.h
+++ llvm/lib/Target/GameBoy/GameBoyRegisterInfo.h
@@ -14,18 +14,20 @@ public:
 
 public:
     // Function stubs live here.
-    /*
     const uint16_t *getCalleeSavedRegs(const MachineFunction *MF = nullptr) const override;
-    const uint32_t *getCallPreservedMask(const MachineFunction &MF, CallingConv::ID CC) const override;
+    
     BitVector getReservedRegs(const MachineFunction &MF) const  override;
-
-    const TargetRegisterClass *getLargestLegalSuperClass(const TargetRegisterClass *RC, const MachineFunction &MF) const override;
-
+    
     /// Stack Frame Processing Methods
     void eliminateFrameIndex(MachineBasicBlock::iterator MI, int SPAdj, 
                              unsigned FIOperandNum, RegScavenger *RS = nullptr) const override;
 
     Register getFrameRegister(const MachineFunction &MF) const override;
+    /*
+    const uint32_t *getCallPreservedMask(const MachineFunction &MF, CallingConv::ID CC) const override;
+
+    const TargetRegisterClass *getLargestLegalSuperClass(const TargetRegisterClass *RC, const MachineFunction &MF) const override;
+
 
     const TargetRegisterClass *getPointerRegClass(const MachineFunction &MF, unsigned Kind = 0) const override;
 
